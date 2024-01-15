5de1e7c6bc814976705c74659015f0e0ef6498f5
Ángela Ávila Lasanta <lasantavispa@gmail.com>
2024-01-15 12:21:38 +0100

    some weekend changes

--- js/main.js
+++ js/main.js
@@ -5,34 +5,41 @@ const btnSearch = document.querySelector('.js-btnSearch');
 const inputTask = document.querySelector('.js-inputTask');
 const task = document.querySelector('.js-task');
 const ul = document.querySelector('.js-ul');
-const li = `<li class="fieldsetTwo__div js-li">
-<input type="checkbox" class="fieldsetTwo__div--check js-inputTask">
-<span class="fieldsetTwo__div--parrag js-task">${arrayInputs}.</span>
-</li>`;
 const arrayInputs = [];
-
+const inputVale = inputAdd.value;
 
 const tasks = [
-    { name: 'Recoger setas en el campo', completed: true },
-    { name: 'Comprar pilas', completed: true },
-    { name: 'Poner una lavadora de blancos', completed: true },
-    {
-      name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
-      completed: false,
-    },
-  ];
-  // Pintar una tarea:
-  // 1.- clase = lineThrough
-  // 2.- crear una funcion
-  // 3.- if completed: true te devuelte la tarea con lineThrough
-  // función para pintar en el html
-  // Recogemos el input.value , y lo pegamos en ul
+  { name: 'Recoger setas en el campo', completed: true },
+  { name: 'Comprar pilas', completed: true },
+  { name: 'Poner una lavadora de blancos', completed: true },
+  { name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
+    completed: false,
+  },
+];
+// Pintar una tarea:
+// 1.- clase = lineThrough
+// 2.- crear una funcion
+// 3.- if completed: true te devuelte la tarea con lineThrough
+// función para pintar en el html
+// Recogemos el input.value , y lo pegamos en ul
 
-function printTask () {
-  ul.innerHTML = tasks[1];
+function takeValue() {
+  let newTask = document.getElementById('js-newTask').value;
+  let newObject = { name: newTask, completed: false };
+  tasks.push(newObject);
 }
-btnAdd.addEventListener('click', printTask);
 
-function handlePrint() {
-  ul.innerHTML = tasks[1];
+for (const eachName of tasks) {
+  ul.innerHTML += `<li class="fieldsetTwo__div js-li">
+    <input type="checkbox" onclick="crossed()" class="fieldsetTwo__div--check js-inputTask">
+    <span class="fieldsetTwo__div--parrag js-task"> ${eachName.name}.</span>
+    </li>`;
 }
+
+function crossed(event) {
+  event.preventDefault();
+  task.classList.add('lineThrough');
+}
+inputTask.addEventListener('click', takeValue);
+
+// primero cojo el input value y lo pego en
